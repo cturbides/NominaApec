7828bd87c1e728c997674f0f093f73cd649ee6d9
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2024-06-07 00:38:50 +0000

    Updated employees application with basic empty CRUD

--- nomina/employees/models.py
+++ nomina/employees/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from positions.models import Position
 
-# Create your models here.
+class Employee(models.Model):
+    id = models.AutoField(verbose_name='employee_id', primary_key=True)
+    name = models.CharField(verbose_name='employee_name', max_length=100)
+    national_id = models.CharField(verbose_name='national_id', max_length=20, unique=True)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, verbose_name='position')
+    mensual_salary = models.DecimalField(verbose_name='mensual_salary', decimal_places=2, max_digits=11)
+
+    def __str__(self):
+        return self.name
