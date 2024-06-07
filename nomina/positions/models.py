031613d0716badb1bf7675efe4f88ad7dc0dbf80
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2024-06-07 00:54:56 +0000

    Updated position model with new validation for maximum_salary and minimum_salary

--- nomina/positions/models.py
+++ nomina/positions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 class Position(models.Model):
     id = models.AutoField(verbose_name='position_id', auto_created=True, primary_key=True)
@@ -6,6 +7,14 @@ class Position(models.Model):
     risk_level = models.SmallIntegerField(verbose_name='position_risk_level')
     minimum_salary = models.DecimalField(verbose_name='position_minimum_salary', decimal_places=2, max_digits=11)
     maximum_salary = models.DecimalField(verbose_name='position_maximum_salary', decimal_places=2, max_digits=11)
+
+    def clean(self):
+        super().clean()
+        if self.minimum_salary >= self.maximum_salary:
+            raise ValidationError({
+                'minimum_salary': 'Minimum salary must be less than maximum salary.',
+                'maximum_salary': 'Maximum salary must be greater than minimum salary.',
+            })
 
     def __str__(self):
         return self.name
