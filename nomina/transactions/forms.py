c676729a72771b5d1c6d7d900f15c32802704c1b
Carlos Sanchez <c.sturbides@gmail.com>
2024-08-09 03:23:04 -0400

    Updated transactions templates

--- nomina/transactions/forms.py
+++ nomina/transactions/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from .models import Transaction
+from django.utils import timezone
 
 class TransactionForm(forms.ModelForm):
+    datetime = forms.DateTimeField(
+        initial=timezone.now,
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'})
+    )
+
     class Meta:
         model = Transaction
-        fields = ['employee', 'datetime', 'amount', 'income', 'deduction', 'transaction_type', 'state']
+        fields = ['employee', 'datetime', 'amount', 'income', 'deduction', 'state']
 
         widgets = {
             'income': forms.Select,
